30657e5a3ce417805065d6d88e314a4c03ac9189
MarAlfieri <168040618+MarAlfieri@users.noreply.github.com>
2024-08-02 20:41:32 -0300

    Update contacto.js

--- js/contacto.js
+++ js/contacto.js
@@ -5,23 +5,16 @@ let contacto = [];
 let grabar= true; //Variable para habilitar grabación de datos
 //Verifico los click del boton
 document.getElementById("enviar").addEventListener("click", (e) => {
-    for (let index = 0; index < entradas.length; index++) {
-        contacto[index] =" "+entradas[index].value;
-        if (entradas[index].value=="" && grabar) {
-            grabar=false;
+    //Si no se cumplen con las condiciones de validación, no crea el archivo
+    if (document.querySelector("form").reportValidity()){
+        //Creo el archivo de texto con los datos del form
+        for (let index = 0; index < entradas.length; index++) {
+            contacto[index] =" "+entradas[index].value;
         }
-    }
-    //Si no se cumplen con las condiciones de validación, no se graba el archivo
-    if (grabar) {
         //Se crea el archivo de texto
         let mensaje = new Blob([contacto], {type: "text/plain;charset=utf-8"}); // navegador
         //Libreria FileSaver.js
         saveAs(mensaje, "contacto.txt");
         alert("Gracias por su mensaje!!. A la brevedad nos pondremos en contacto.")
-    } else {
-        grabar=true;
     }
 })
-
-
-
